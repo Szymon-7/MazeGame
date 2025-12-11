47632f3e30a1c77cf74961cbc34e831d8c8dca62
Szymon Patryn <szymonpatryn@gmail.com>
2025-12-10 23:55:32 -0500

    Project Restructure - actual build tool with Maven

---
+++ src/main/java/Cell.java
@@ -0,0 +1,13 @@
+public class Cell {
+   boolean top = true, bottom = true, left = true, right = true;
+   boolean visited = false;
+   boolean visible = false;
+   boolean hasCoin = false;
+
+   int row, col;
+
+   public Cell(int row, int col) {
+      this.row = row;
+      this.col = col;
+   }
+}
